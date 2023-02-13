b37c812c99a224071caf0df5c282a4eba0303d68
Quan-Ngo <71655929+Quan-Ngo@users.noreply.github.com>
2023-02-13 14:32:49 -0500

    buffdebuff manager

---
+++ Assets/Script/BuffDebuffManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum BuffAndDebuff {DAMAGEUP, LIFESTEAL, DAMAGEDOWN, SHIELDBREAK}
+
+public class BuffDebuffManager : MonoBehaviour
+{
+	public static BuffDebuffManager instance;
+	
+	Dictionary<BuffAndDebuff, int> playerBuffDebuff = new Dictionary<BuffAndDebuff, int>();
+	Dictionary<BuffAndDebuff, int> bossBuffDebuff = new Dictionary<BuffAndDebuff, int>();
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
