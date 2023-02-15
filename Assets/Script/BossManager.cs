873d5a7e4e9380acf2943492b2e64881efa7aad1
Quan-Ngo <71655929+Quan-Ngo@users.noreply.github.com>
2023-02-15 18:11:02 -0500

    prototype AI and health tracking for boss

--- Assets/Script/BossManager.cs
+++ Assets/Script/BossManager.cs
@@ -5,7 +5,23 @@ using UnityEngine;
 public class BossManager : MonoBehaviour
 {
     public static BossManager Instance;
-
+	public int phaseOneThreshholdPercent;
+	public int phaseTwoThreshholdPercent;
+	public int phaseThreeThreshholdPercent;
+	public int maxBossHealth;
+	
+	
+	[SerializeField]
+	private int bossHealth;
+	private int bossPhase;
+	
+	
+	void Start()
+	{
+		bossPhase = 0;
+		bossHealth = maxBossHealth;
+	}
+	
     void Awake()
     {
         if (Instance == null)
@@ -18,17 +34,59 @@ public class BossManager : MonoBehaviour
         }
     }
 
-    public void testAttack()
+    /*public void testAttack()
     {
         PlayerManager.Instance.takeDamage(15);
         Debug.Log("Rawr, Die");
         TurnManager.Instance.changeTurn();
-    }
+    }*/
 
     public void turnStart()
     {
-        testAttack();
+        //testAttack();
+		switch (bossPhase)
+		{
+			case 0:
+				phaseZeroAI();
+				break;
+			case 1:
+				//phaseOneAI();
+				break;
+			case 2:
+				//phaseTwoAI();
+				break;
+			case 3:
+				//phaseThreeAI();
+				break;
+		}
     }
 
-
+	void phaseZeroAI()
+	{
+		attackPlayer();
+	}
+	
+	void attackPlayer()
+	{
+		PlayerManager.Instance.takeDamage(5);
+		TurnManager.Instance.changeTurn();
+	}
+	
+	void takeDamage(int amount)
+	{
+		bossHealth -= amount;
+		if (bossHealth <= (int) (maxBossHealth * (phaseThreeThreshholdPercent/100)))
+		{
+			bossPhase = 2;
+		}
+		else if (bossHealth <= (int) (maxBossHealth * (phaseTwoThreshholdPercent/100)))
+		{
+			bossPhase = 2;
+		}
+		else if (bossHealth <= (int) (maxBossHealth * (phaseOneThreshholdPercent/100)))
+		{
+			bossPhase = 1;
+		}
+		
+	}
 }
