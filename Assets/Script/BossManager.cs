e1ab986862ecb86fa0e81d7fbf720b52ff1fb3fc
Quan-Ngo <71655929+Quan-Ngo@users.noreply.github.com>
2023-03-01 20:44:38 -0500

    put boss main AI functions into coroutines for timing purposes

--- Assets/Script/BossManager.cs
+++ Assets/Script/BossManager.cs
@@ -16,6 +16,7 @@ public class BossManager : MonoBehaviour
 	public TMP_Text HPDisplay;
 	public TMP_Text dmgDisplay;
 	public Animator animator;
+	public Animator phaseChangeFX;
 	
 	
 	[SerializeField] private int bossHealth;
@@ -57,67 +58,82 @@ public class BossManager : MonoBehaviour
 		
 		if (!enraged)
 		{
-			if (phaseTransition == true)
-			{
-				changePhase();
-				phaseTransition = false;
-			}
-			
-			switch (bossPhase)
-			{
-				case 0:
-					phaseZeroAI();
-					break;
-				case 1:
-					phaseOneAI();
-					break;
-				case 2:
-					phaseTwoAI();
-					break;
-				case 3:
-					StartCoroutine(phaseThreeAI());
-					break;
-			}
+			StartCoroutine(normalAI());
 		}
 		else
 		{
 			StartCoroutine(enragedAI());
-		}
-		TurnManager.Instance.changeTurn();
+		}	
     }
+	
+	IEnumerator normalAI()
+	{
+		if (phaseTransition == true)
+		{
+			StartCoroutine(changePhase());
+			phaseTransition = false;
+			yield return new WaitForSeconds(1.7f);
+		}
+		
+		switch (bossPhase)
+		{
+			case 0:
+				StartCoroutine(phaseZeroAI());
+				break;
+			case 1:
+				StartCoroutine(phaseOneAI());
+				break;
+			case 2:
+				StartCoroutine(phaseTwoAI());
+				break;
+			case 3:
+				StartCoroutine(phaseThreeAI());
+				break;
+		}
+	}
 
-	void phaseZeroAI()
+
+	IEnumerator phaseZeroAI()
 	{
 		attackPlayer();
+		yield return new WaitForSeconds(1.2f);
+		TurnManager.Instance.changeTurn();
 	}
 	
-	void phaseOneAI()
+	IEnumerator phaseOneAI()
 	{
 		switch (Random.Range(0, 2))
 		{
 			case 0:
 				attackPlayer();
+				yield return new WaitForSeconds(1.2f);
 				break;
 			case 1:
 				StartCoroutine(buffSelfDamage(1));
+				yield return new WaitForSeconds(1f);
 				break;
 		}
+		TurnManager.Instance.changeTurn();
 	}
 	
-	void phaseTwoAI()
+	IEnumerator phaseTwoAI()
 	{
 		switch (Random.Range(0, 3))
 		{
 			case 0:
 				attackPlayer();
+				yield return new WaitForSeconds(1.2f);
 				break;
 			case 1:
 				StartCoroutine(buffSelfDamage(1));
+				yield return new WaitForSeconds(1f);
 				break;
 			case 2:
 				StartCoroutine(debuffPlayerDamage(1));
+				yield return new WaitForSeconds(1f);
 				break;
 		}
+		TurnManager.Instance.changeTurn();
 	}
 	
 	IEnumerator phaseThreeAI()
@@ -125,6 +141,8 @@ public class BossManager : MonoBehaviour
 		StartCoroutine(buffSelfDamage(1));
 		yield return new WaitForSeconds(1f);
 		attackPlayer();
+		yield return new WaitForSeconds(1.2f);
+		TurnManager.Instance.changeTurn();
 	}
 	
 	IEnumerator enragedAI()
@@ -132,6 +150,8 @@ public class BossManager : MonoBehaviour
 		StartCoroutine(buffSelfDamage(5));
 		yield return new WaitForSeconds(1f);
 		attackPlayer();
+		yield return new WaitForSeconds(1.2f);
+		TurnManager.Instance.changeTurn();
 	}
 	
 	
@@ -176,8 +196,11 @@ public class BossManager : MonoBehaviour
 		}
 	}
 	
-	void changePhase()
+	IEnumerator changePhase()
 	{
+		phaseChangeFX.SetTrigger("Start");
+		animator.SetTrigger("Cast");
+		yield return new WaitForSeconds(0.7f);
 		switch (bossPhase)
 		{
 			case 1:
